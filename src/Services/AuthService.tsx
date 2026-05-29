b79f303e791e25451b664ed9d8348bc105b4a582
rexxx777 <yashv3937@gmail.com>
2026-05-29 15:31:33 +0530

    update render url

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const base_url = "http://localhost:8080/auth/"
+// const base_url = "http://localhost:8080/auth/"
+const base_url = 'https://backend-0mvg.onrender.com/auth/'
 const loginUser = async (login:any)=> {
     return axios.post(`${base_url}login`, login)
         .then((result:any) => result.data)
